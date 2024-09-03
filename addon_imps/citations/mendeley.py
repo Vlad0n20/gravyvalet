c3a2efb61d010275fce537285bf4f34d750025b7
Andriy Sheredko <sheredko.andriy@gmail.com>
2024-09-03 13:41:43 +0300

    - ENG-5989, implemented Mendeley API for Citations Interface + added citations support throughout the code

--- addon_imps/citations/mendeley.py
+++ addon_imps/citations/mendeley.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from addon_toolkit.interfaces.citation import (
     CitationAddonImp,
     ItemResult,
@@ -8,12 +10,17 @@ from addon_toolkit.interfaces.citation import (
 
 class MendeleyCitationImp(CitationAddonImp):
     async def get_external_account_id(self, auth_result_extras: dict[str, str]) -> str:
-        return auth_result_extras["userID"]
+        async with self.network.GET(
+            "profiles/me",
+        ) as response:
+            profile_info = await response.json_content()
+            user_id = profile_info.get("id")
+            if not user_id:
+                raise KeyError("Failed to fetch user ID from Mendeley.")
+            return str(user_id)
 
     async def list_root_collections(self) -> ItemSampleResult:
-        async with self.network.GET(
-            f"{self.config.external_api_url}/folders"
-        ) as response:
+        async with self.network.GET("folders") as response:
             response_json = await response.json_content()
             return self._parse_collection_response(response_json)
 
@@ -21,79 +28,62 @@ class MendeleyCitationImp(CitationAddonImp):
         self,
         collection_id: str,
         filter_items: ItemType | None = None,
-        page_cursor: str = "",
     ) -> ItemSampleResult:
         async with self.network.GET(
-            f"{self.config.external_api_url}/folders/{collection_id}/documents",
-            query=self._params_from_cursor(page_cursor),
+            f"folders/{collection_id}/documents",
         ) as response:
-            response_json = await response.json_content()
-            return self._parse_items_response(response_json, filter_items)
+            import pydevd_pycharm
 
-    # async def add_new_item(self, collection_id: str, item_data: dict) -> ItemResult:
-    #     async with self.network.POST(f"{self.config.external_api_url}/folders/{collection_id}/documents", json=item_data) as response:
-    #         new_item = await response.json_content()
-    #         return ItemResult(
-    #             item_id=int(new_item['id']),
-    #             item_name=new_item['title'],
-    #             item_type=ItemType.DOCUMENT,
-    #             csl=new_item.get("csl", {})
-    #         )
-    #
-    # async def delete_item(self, item_id: str) -> None:
-    #     async with self.network.DELETE(f"{self.config.external_api_url}/documents/{item_id}") as response:
-    #         if response.status_code != 204:
-    #             raise Exception("Failed to delete item")
+            pydevd_pycharm.settrace(
+                "host.docker.internal",
+                port=12346,
+                stdoutToServer=True,
+                stderrToServer=True,
+            )
+            document_ids = await response.json_content()
+
+            items = await self._fetch_documents_details(document_ids, filter_items)
+
+            return ItemSampleResult(items=items, total_count=len(items))
+
+    async def _fetch_documents_details(
+        self, document_ids: list[dict], filter_items: ItemType | None
+    ) -> list[ItemResult]:
+        items = []
+
+        async def fetch_item_details(item_id):
+            async with self.network.GET(f"documents/{item_id}") as item_response:
+                item_details = await item_response.json_content()
+                item_name = item_details.get("title", f"Untitled Document {item_id}")
+                item_type = item_details.get("type", "unknown")
+
+                if filter_items is None or filter_items == item_type:
+                    items.append(
+                        ItemResult(
+                            item_id=item_id,
+                            item_name=item_name,
+                            item_type=item_type,
+                            item_path=None,
+                            csl=item_details.get("csl", {}),
+                        )
+                    )
+
+        tasks = [fetch_item_details(doc["id"]) for doc in document_ids]
+        await asyncio.gather(*tasks)
+
+        return items
 
     def _parse_collection_response(self, response_json: dict) -> ItemSampleResult:
+        print(response_json, flush=True)
         items = [
             ItemResult(
-                item_id=int(collection["id"]),
+                item_id=collection["id"],
                 item_name=collection["name"],
-                item_type=ItemType.COLLECTION,
+                item_type="COLLECTION",
                 item_path=None,
                 csl=None,
             )
-            for collection in response_json.get("folders", [])
+            for collection in response_json
         ]
-        return ItemSampleResult(items=items, total_count=len(items))
 
-    def _parse_items_response(
-        self, response_json: dict, filter_items: ItemType | None
-    ) -> ItemSampleResult:
-        items = [
-            ItemResult(
-                item_id=int(item["id"]),
-                item_name=item["title"],
-                item_type=(
-                    ItemType.DOCUMENT
-                    if item["type"] == "document"
-                    else ItemType.COLLECTION
-                ),
-                item_path=None,
-                csl=item.get("csl", {}),
-            )
-            for item in response_json.get("documents", [])
-            if filter_items is None
-            or (filter_items == ItemType.DOCUMENT and item["type"] == "document")
-        ]
         return ItemSampleResult(items=items, total_count=len(items))
-
-    # def _params_from_cursor(self, cursor: str = "") -> dict[str, str]:
-    #     if cursor:
-    #         offset_cursor = OffsetCursor.from_str(cursor)
-    #         return {"start": offset_cursor.offset, "limit": offset_cursor.limit}
-    #     return {}
-    #
-    # def _parse_cursor(self, headers: dict[str, str]) -> OffsetCursor:
-    #     total_count = int(headers.get('Total-Results', 0))
-    #     offset = int(headers.get('Start', 0))
-    #     limit = int(headers.get('Items-Per-Page', 50))
-    #     return OffsetCursor(offset=offset, limit=limit, total_count=total_count)
-    #
-    # @functools.cache
-    # def _root_collection_id(self) -> str:
-    #     return "root"
-    #
-    #
-    # m = MendeleyCitationImp()
