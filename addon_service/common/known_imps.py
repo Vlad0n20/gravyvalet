158c3c7b5e361ab479cce9d11444ff7521f344ba
Oleh Paduchak <opaduchak@exoft.net>
2024-09-03 18:27:16 +0300

    Figshare imp

--- addon_service/common/known_imps.py
+++ addon_service/common/known_imps.py
@@ -8,6 +8,7 @@ import enum
 from addon_imps.citations import zotero_org
 from addon_imps.storage import (
     box_dot_com,
+    figshare,
     google_drive,
     dropbox,
     s3,
@@ -63,6 +64,7 @@ class KnownAddonImps(enum.Enum):
     S3 = s3.S3StorageImp
     ZOTERO_ORG = zotero_org.ZoteroOrgCitationImp
     GOOGLE_DRIVE = google_drive.GoogleDriveStorageImp
+    FIGSHARE = figshare.FigshareStorageImp
     DROPBOX = dropbox.DropboxStorageImp
 
     if __debug__:
@@ -78,8 +80,8 @@ class AddonImpNumbers(enum.Enum):
     ZOTERO_ORG = 1002
     S3 = 1003
     GOOGLE_DRIVE = 1005
-
     DROPBOX = 1006
+    FIGSHARE = 1007
 
     if __debug__:
         BLARG = -7
